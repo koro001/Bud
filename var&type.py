ede05d2fdf014ffdda6e7c67798652701ac3f3f3
koro001 <144470661+koro001@users.noreply.github.com>
2025-01-14 12:02:42 +0530

    first commit

---
+++ var&type.py
@@ -0,0 +1,37 @@
+
+myfloat=float(7.5646)
+print(myfloat)
+
+mystring="pa how are u"
+print(mystring)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a,b=7,2
+print(a,b)
+
+#exercise
+
+mystring="hello"
+print(mystring)
+
+myfloat=float(10.10)
+print(myfloat)
+
+myint=int(20.22)
+print(myint)
+
+mystring="hello"
+myfloat=20.1
+myint=10
+
+x = 55.5
+print(isinstance(x, (int, float)))  
